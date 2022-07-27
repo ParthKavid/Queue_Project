f984aeea40a6fe44efc6623afb60be3dee6049a2
ParthKavid <parthkavid@gmail.com>
2022-07-26 22:41:09 -0400

    Add project files.

---
+++ Controllers/Doctor/DoctorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Queue_Project.Controllers.Doctor
+{
+    public class DoctorController : Controller
+    {
+        // GET: Doctor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Doctor()
+        {
+
+            return View();
+        }
+    }
+}
